128d65a9d28b112ba4cef3813af804cbbd13548a
azeemnitw <azeemmohd.azeem752@gmail.com>
2019-02-10 21:18:56 +0530

    new

--- TestingNg/src/test/Ultimate.java
+++ TestingNg/src/test/Ultimate.java
@@ -14,25 +14,9 @@ public class Ultimate {
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver","C://chromedriver.exe" );
-		WebDriver d=new ChromeDriver();
-		d.get("https://www.calculator.net/age-calculator.html");
-		Select s=new Select(d.findElement(By.xpath("//select[@id='today_Month_ID']")));
-		s.selectByValue("11");
-		TimeUnit.SECONDS.sleep(2);
-		Select q=new Select(d.findElement(By.xpath("//select[@id='today_Day_ID']")));
-		q.selectByValue("29");
-		WebElement e=d.findElement(By.xpath("//input[@id='today_Year_ID']"));
-		e.sendKeys(Keys.CONTROL+"a");
-		e.sendKeys(Keys.DELETE);
-		e.sendKeys("2000");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.goindigo.in/");
 		
-		
-		//d.findElement(By.linkText("Sign in")).click();
-		//d.findElement(By.xpath("//input[@class='whsOnd zHQkBf']")).sendKeys("azeemmohd.azeem752@gmail.com");
-		//d.findElement(By.linkText("Next")).click();
-		
-		
-		// TODO Auto-generated method stub
 		
 
 
